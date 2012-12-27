ed4e722c7004cd0ee12e098d9dadbaf883d885ae
maojj <maojj@rd.netease.com>
2012-12-27 18:22:58 +0800

    init: from http://www.sunsetlakesoftware.com/molecules,  Latest source code release: 3/30/2012

---
+++ SLSAtomColorKeyController.h
@@ -0,0 +1,14 @@
+//
+//  SLSAtomColorKeyController.h
+//  Molecules
+//
+//  The source code for Molecules is available under a BSD license.  See License.txt for details.
+//
+
+#import <UIKit/UIKit.h>
+#import <QuartzCore/QuartzCore.h>
+#import "SLSMolecule.h"
+
+@interface SLSAtomColorKeyController : UITableViewController
+
+@end
